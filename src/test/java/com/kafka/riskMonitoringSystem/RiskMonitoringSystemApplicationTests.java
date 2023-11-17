86a0d5f2b04406bcada712852e69d9d9cbeb5c2c
shizo1o1 <121438034+shizo1o1@users.noreply.github.com>
2023-11-17 20:55:31 +0600

    create project kafka

---
+++ src/test/java/com/kafka/riskMonitoringSystem/RiskMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.riskMonitoringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
